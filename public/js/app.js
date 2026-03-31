3d1212aac134d6111b3e6937da743c96e7aa0514
Oldrich Vetesnik <oldrich.vetesnik@gmail.com>
2026-03-31 17:15:49 +0200

    Fix components initialization

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,20 @@
 function initComponents() {
-  let componentsMap = {}
+  let componentsMap = new Set()
 
   $("[data-js-component]").each((_, item) => {
     const $item = $(item)
     let componentNames = $item.data("js-component").split(",").map(name => name.trim())
 
-    componentNames.forEach(componentName => {
-      componentsMap[componentName] = true
-    })
-
     for (const componentName of componentNames) {
+      if (componentsMap.has(componentName)) {
+        continue
+      }
+
+      componentsMap.add(componentName)
+
       const component = window[componentName]
       console.log(`Initializing ${componentName}`)
+
       try {
         new component
       } catch (e) {
